320cc5d2d83af76782dc6d02b86b0efa727b7b74
PatoJZ <patojimenez08@gmail.com>
2023-04-30 16:26:34 -0400

    ejercicios completos

--- Labsemana7_ejercicios.cpp
+++ Labsemana7_ejercicios.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <list>
 
 using namespace std;
 
@@ -44,17 +45,67 @@ void returnstringlenght(){
     getline(cin,str);
     cout<< "la longitd es "<< str.length() << endl;
 }
-void numberarray(){
-    int 
+void aritmeticOperations(){
+    list<int> numeros;
+    int n;
+    cout << "Ingrese el número de elementos: ";
+    cin >> n;   
+    for (int i = 0; i < n; i++) {
+        int num;
+        cout << "Ingrese el elemento " << i+1 << ": ";
+        cin >> num;
+        numeros.push_back(num);
+    }
+    int suma = 0;
+    int producto = 1;
+    double promedio = 0;       
+    for (int num : numeros) {
+      suma += num;
+      producto *= num;
+    }
 
+    promedio = (double) suma / numeros.size();
 
-
+    cout << "La suma es: " << suma << endl;
+    cout << "El producto es: " << producto << endl;
+    cout << "El promedio es: " << promedio << endl;
 }
+int Factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * Factorial(n - 1);
+    }
+}
+int fibonacci(int n) {
+    if (n == 0 || n == 1) {
+        return n;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+bool Palindromo(string palabra) {
+    // Eliminar espacios y convertir todo a minúsculas
+    string palabraLimpia = "";
+    for (int i = 0; i < palabra.length(); i++) {
+        if (isalpha(palabra[i])) {
+            palabraLimpia += tolower(palabra[i]);
+        }
+    }
 
+    // Comprobar si la palabra es un palíndromo
+    int i = 0;
+    int j = palabraLimpia.length() - 1;
+    while (i < j) {
+        if (palabraLimpia[i] != palabraLimpia[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
 int main(){
-
-    cout << "hola" << endl;
-    cout << "hola" << endl;
     //ejercicio1
     getintegerinformaction();
     //ejercicio2
@@ -62,6 +113,31 @@ int main(){
     //ejercicio3
     returnstringlenght();
     //ejercicio4
+    aritmeticOperations();
+    //ejericicio5
+    int NumFactorial;
+    cout << "Ingrese un número entero: ";
+    cin >> NumFactorial;
+    cout << "El factorial de " << NumFactorial << " es " << Factorial(NumFactorial) << endl;
+    //ejercicio6
+    int numFibonacci;
+    cout << "Introduzca el número de términos de la secuencia de Fibonacci a calcular: ";
+    cin >> numFibonacci;
+    
+    cout << "La secuencia de Fibonacci con " << numFibonacci << " términos es: ";
+    for (int i = 0; i < numFibonacci; i++) {
+        cout << fibonacci(i) << " ";
+    }
+    //ejercicio 7
+    string palabra;
+    cout << "Ingrese una palabra u oración: ";
+    getline(cin, palabra);
+
+    if (Palindromo(palabra)) {
+        cout << palabra << " es un palíndromo." << endl;
+    } else {
+        cout << palabra << " no es un palíndromo." << endl;
+    }
 
 
 
